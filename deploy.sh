a1ffb9227e9565b4aa895b0fa6a6a9bbd3cc2883
Erik Wijmans <ewijmans2@gmail.com>
2018-02-20 00:03:24 -0500

    Fix

--- deploy.sh
+++ deploy.sh
@@ -25,6 +25,8 @@ rm -rf public/*
 echo "Generating site"
 hugo
 
+cp -v CNAME public/CNAME
+
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)" && cd ..
 git push origin master
